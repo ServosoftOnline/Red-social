9cde7a8613c6059f2a3148c7f240768530d59fab
servosoftonline <oscarfernandezsantiago@gmail.com>
2025-09-03 23:45:39 +0200

    Añado el frontend de pruebas

--- controllers/publication.js
+++
@@ -1,6 +0,0 @@
-export const pruebaPublication = (req,res) => {
-    return res.status(200).json({
-        status: 200,
-        message: "Soy una acción de prueba desde: controllers/publication.js"
-    });
-}
--- controllers/user.js
+++
@@ -1,7 +0,0 @@
-// Método de prueba
-export const pruebaUser = (req,res) => {
-    return res.status(200).json({
-        status: 200,
-        message: "Soy una acción de prueba desde: controllers/user.js"
-    });
-}
